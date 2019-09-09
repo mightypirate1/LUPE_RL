b2bee3d1c5b74b3d68d7c6c322052e5af2a59804
Martin Frisk <yfflan@gmail.com>
2019-09-09 20:19:12 +0200

    added normalization, so that agents gets a state that is in some sense without scale

--- Trading/RL/env.py
+++ Trading/RL/env.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 settings = {
-            "episode_length" : 7,
-            "state_length"   : 32
+            "episode_length" : 128,
+            "state_length"   : 128
             }
 
 class databank:
@@ -14,16 +14,19 @@ class databank:
         self.state_len = settings["state_length"]
         self.ep_len = episode_length
         self.data = pd.read_csv(file, delimiter=',').dropna()
-        self.mean_vol = self.data["Volume"].mean()
         self.reset()
 
     def reset(self):
         self.t = np.random.randint(self.state_len, high=self.data.shape[0] - self.ep_len)
+        self.mean_vol = self.data["Volume"].mean()
+        self.init_price = self.data.iloc[self.t]["Open"]
+
     def step(self):
         if self.t < self.end:
             self.t += 1
             return True
         return False
+
     def statemaker(self, s):
         components = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
         ret = s.copy(deep=True)[components]
@@ -33,65 +36,76 @@ class databank:
                 ret[c] = ret[c] / mean_close
         ret["Volume"] = ret["Volume"] / self.mean_vol
         return ret
+
     def get_state(self):
-        return self.statemaker( self.data.iloc[self.t-self.state_len:self.t] )
+        return self.statemaker( self.data.iloc[self.t-self.state_len:self.t]) / self.mean_price
+
     @property
     def end(self):
         return self.data.shape[0]
+
     @property
     def price(self):
         return self.data.iloc[self.t]["Close"]
 
-class account:
-    def __init__(self):
-        self.price = 10 # price of stock
-        self.stock_count = 100 # n stock owned
-        self.cash = 10 * self.price # initial cash
+    @property
+    def mean_price(self):
+        return self.data.iloc[self.t-settings["state_length"]:self.t]["Close"].mean()
 
-    def reset(self):
-        pass
+class account:
+    def __init__(self, market):
+        self.market = market
+        self.reset()
+
+    def reset(self, max_stock=10, max_cash=10):
+        #reset account at a given price. you get unif(1,max_stock) stocks, and cash enough to buy another unif(1,max_cash)
+        price = self.init_price = self.market.price # price of stock
+        self.stock_count = np.random.randint(1, high=max_stock+1)
+        self.cash = np.random.randint(1, high=max_cash) * self.market.price
 
     def buy(self, n):
-        if self.cash < n * price:
+        if self.cash < n * self.market.price:
             return
-        self.cash -= self.price * n
+        self.cash -= self.market.price * n
         self.stock_count += n
         return
     def sell(self, n):
         m = min(n,self.stock_count)
-        self.cash += m*price
+        self.cash += self.market.price * m
         self.stock_count -= m
 
     def get_state(self):
-        return np.array([self.cash, self.stock_count])
+        return np.array([self.cash / self.market.price, self.stock_count ])
+
     @property
     def value(self):
-        return self.cash + self.price * self.stock_count
+        return self.cash + self.market.price * self.stock_count
 
 class env:
     def __init__(self):
         self.market = databank()
-        self.customer = account()
+        self.customer = account(self.market)
         self.reset()
     def reset(self):
-        self.customer.reset()
         self.market.reset()
+        self.customer.reset()
         self.t = 0
         return self.get_state()
     def get_state(self):
         return {"customer" : self.customer.get_state(), "market" : self.market.get_state()}
     def perform_action(self, a):
         val = self.customer.value
+        self.customer.price = self.market.price
+
         if a == 0: #wait
             pass
         elif a == 1: #buy
-            self.customer.price = self.market.price
             self.customer.buy(1)
         elif a == 2: #sell
-            self.customer.price = self.market.price
             self.customer.sell(1)
+
         self.t += 1
         self.market.step()
         r = (self.customer.value - val) / self.market.price
-        d = False if t < settings["episode_length"] else True
-        return r, self.get_state, d
+        d = False if self.t < settings["episode_length"] else True
+        return r, self.get_state(), d
