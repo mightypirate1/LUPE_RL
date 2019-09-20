11057238f72d399e0b23aa41615fa6e982cdde33
Martin Frisk <yfflan@gmail.com>
2019-09-20 22:08:45 +0200

    some small fix to script

--- Trading/RL/scratchpaper.py
+++ Trading/RL/scratchpaper.py
@@ -3,24 +3,30 @@ import tensorflow as tf
 from env import env
 from agents.dqn import agent
 
-load = False
+###
+load  = None
 train = True
-eval = False
-demo = False
+eval  = True
+demo  = False
+
+###
+# load  = "ape-900000"
+# train = False
 
 e = env()
 # e = env(file="LUPE.ST.csv")
 
 with tf.Session() as session:
-    a = agent(session, env=e)
+    a = agent( "ape", session, env=e)
     session.run(tf.global_variables_initializer())
-    if load:
-        exit("not implemented")
+    if load is not None:
+        a.load_model(load)
     if train:
         a.train(1000000)
     if eval:
+        stats = a.evaluate_model(10000)
+    if demo:
         a.env = env(interactive=True)
         print("Current evaluation is:")
         print(a.current_eval)
-    if demo:
         a.env.reset()
